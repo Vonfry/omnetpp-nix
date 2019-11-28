bd049dcca2931975a1cd7d4eb9884509ac5ca816
Vonfry <mail@vonfry.name>
2019-11-28 18:46:31 +0800

    make configure working, now problem is building

--- default.nix
+++ default.nix
@@ -1,14 +1,14 @@
 let pkgs = import <nixpkgs> {};
 in
 { stdenv                ? pkgs.stdenv
-, src                   ? ./omnetpp
-, clang                 ? pkgs.clang
+, gawk                  ? pkgs.gawk
+, which                 ? pkgs.which
 , bison                 ? pkgs.bison
 , flex                  ? pkgs.flex
 , perl                  ? pkgs.perl
 , python                ? pkgs.python
 , python3               ? pkgs.python3
-, qt5                   ? pkgs.qt5
+, qtbase                ? pkgs.qt5.qtbase
 , libsForQt5            ? pkgs.libsForQt5
 , tcl                   ? pkgs.tcl
 , tk                    ? pkgs.tk
@@ -18,42 +18,53 @@ in
 , webkitgtk             ? pkgs.webkitgtk
 , enable3dVisualization ? false
 , openscenegraph        ? pkgs.openscenegraph
-, enableParallel        ? true
+, enableParallel        ? false
 , openmpi               ? pkgs.openmpi
 , enablePCAP            ? false
 , libpcap               ? pkgs.libpcap
 , doxygen               ? pkgs.doxygen
+, inkscape              ? pkgs.inkscape
 , zlib                  ? pkgs.zlib
+, nemiver               ? pkgs.nemiver
+, akaroa                ? null
 }:
 
 assert enable3dVisualization -> openscenegraph != null;
-assert enableParallel -> openmpi != null;
+assert enableParallel -> openmpi != null && akaroa != null;
 assert enablePCAP -> libpcap != null;
 
-stdenv.mkDerivation {
-  name = "omnetpp";
-  src = src;
+stdenv.mkDerivation rec {
+  src = ./omnetpp;
+  name = builtins.replaceStrings [ "\n" ]  [ "" ]
+          (builtins.readFile (src + /Version));
 
-  nativeBuildInputs = [ clang doxygen ];
-  buildInputs = [ bison flex perl python python3 qt5.qtbase tcl tk libxml2
-                  graphviz webkitgtk zlib  jre ]
+  outputs = [ "out" "doc" "dev" ];
+
+  nativeBuildInputs = [ gawk which doxygen qtbase ];
+  buildInputs = [ bison flex perl python python3 tcl libxml2 graphviz perl
+                  qtbase tk inkscape webkitgtk zlib jre nemiver
+                ]
                 ++ (if enable3dVisualization
                     then [ openscenegraph ]
                     else [ ])
                 ++ (if enableParallel
-                    then [ openmpi ]
+                    then [ openmpi akaroa ]
                     else [ ])
                 ++ (if enablePCAP
                     then [ libpcap ]
                     else [ ]);
-  outputs = [ "out" "doc" ];
-  configureFlags = [] ++
-                   (if ! enable3dVisualization
-                    then [ "--WITH_OSGEARTH=no"
-                           "--WITH_OSG=no"
-                         ]
-                    else []) ++
-                   (if ! enableParallel
-                    then [ "--WITH_PARSIM=no" ]
-                    else []);
+
+  patches = [ ./patch.configure ./patch.setenv ./patch.HOME ];
+  configureFlags = [ ]
+                   ++ (if ! enable3dVisualization
+                       then [ "WITH_OSG=no" "WITH_OSGEARTH=no" ]
+                       else [])
+                   ++ (if ! enableParallel
+                       then [ "WITH_PARSIM=no" ]
+                       else []);
+  preConfigure = ''
+    cp configure.user.dist configure.user
+    . setenv
+    export QT_CORE_INCLUDE=" -isystem ${qtbase.dev}/include/QtCore "
+    '';
 }
